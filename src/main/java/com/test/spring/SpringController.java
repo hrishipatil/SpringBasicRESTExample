08cd402d1347249f8fd365111852d827ef7cfe67
hpatil <hrishikeshdinkar.patil@priceline.com>
2016-07-24 23:29:31 -0400

    Added swagger documentation for the sample rest api

---
+++ src/main/java/com/test/spring/SpringController.java
@@ -0,0 +1,31 @@
+package com.test.spring;
+
+import com.wordnik.swagger.annotations.ApiError;
+import com.wordnik.swagger.annotations.ApiErrors;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * Created by hpatil on 6/23/16.
+ */
+
+@Controller
+@RequestMapping("/service")
+public class SpringController {
+
+
+    @RequestMapping(method = {RequestMethod.GET}, value = {"/hello"}, produces = {
+           MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+          MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_XML_VALUE})
+    @ApiOperation(value = "Gets dummy response")
+    @ApiErrors(value = {@ApiError(code = 404, reason = "No resources found")})
+    @ResponseBody
+    public static Dummy search() {
+
+           return new Dummy();
+    }
+}
