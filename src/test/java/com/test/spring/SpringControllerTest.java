240cbeb06fa18f04f90447f3e6a19f83e71190c4
hpatil <hrishikeshdinkar.patil@priceline.com>
2016-12-28 16:29:06 -0500

    added unit test setup for the application

---
+++ src/test/java/com/test/spring/SpringControllerTest.java
@@ -0,0 +1,24 @@
+package com.test.spring;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by hpatil on 28/12/16.
+ */
+public class SpringControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+    @Test
+    public void happy_path_test() {
+        assertTrue(true);
+    }
+
+}
